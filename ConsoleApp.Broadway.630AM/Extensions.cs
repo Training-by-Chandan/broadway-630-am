db1f33c0548ba7ee8b804d531587da273523bdc6
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-09-17 07:49:15 +0545

    Separated Extenstions class

---
+++ ConsoleApp.Broadway.630AM/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using Library1;
+
+namespace ConsoleApp.Broadway._630AM
+{
+    public static class StrinExtension
+    {
+        public static string AddDot(this string s)
+        {
+            return s + ".";
+        }
+    }
+
+    public static class RectangleExtension
+    {
+        public static void FunctionNew(this Rectangle r)
+        {
+        }
+    }
+
+    public static class IntExtension
+    {
+        public static int increasebynum(this int x, int num)
+        {
+            return x + num;
+        }
+    }
+}
--- ConsoleApp.Broadway.630AM/Inheritence.cs
+++ ConsoleApp.Broadway.630AM/Inheritence.cs
@@ -130,27 +130,4 @@ namespace ConsoleApp.Broadway._630AM
         {
         }
     }
-
-    public static class RectangleExtension
-    {
-        public static void FunctionNew(this Rectangle r)
-        {
-        }
-    }
-
-    public static class IntExtension
-    {
-        public static int increasebynum(this int x, int num)
-        {
-            return x + num;
-        }
-    }
-
-    public static class StrinExtension
-    {
-        public static string AddDot(this string s)
-        {
-            return s + ".";
-        }
-    }
 }
