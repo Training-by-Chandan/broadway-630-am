503b0f3bf12612ef4a65780cbe3c582ec5157e37
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-10-03 07:53:30 +0545

    Merging interval problem solved

---
+++ MergingInterval/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergingInterval
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var scenarioOne = new List<Interval>();
+            scenarioOne.Add(new Interval(2, 4));
+            scenarioOne.Add(new Interval(30, 55));
+            scenarioOne.Add(new Interval(3, 15));
+            scenarioOne.Add(new Interval(21, 35));
+            var output = Function(scenarioOne);
+            foreach (var item in output)
+            {
+                Console.Write($"[{item.Lower},{item.Upper}]");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static Stack<Interval> Function(List<Interval> intervals)
+        {
+            var stack = new Stack<Interval>();
+            if (intervals.Count == 0)
+                return stack;
+
+            intervals = intervals.OrderBy(p => p.Lower).ToList();
+            stack.Push(intervals[0]);
+
+            for (int i = 1; i < intervals.Count; i++)
+            {
+                var tempItem = stack.Peek();
+                if (tempItem.Upper > intervals[i].Lower && tempItem.Upper < intervals[i].Upper)
+                {
+                    tempItem.Upper = intervals[i].Upper;
+                }
+                else if (tempItem.Upper < intervals[i].Lower)
+                {
+                    stack.Push(intervals[i]);
+                }
+            }
+
+            return stack;
+        }
+    }
+
+    internal class Interval
+    {
+        public Interval(int lower, int upper)
+        {
+            this.Lower = lower;
+            this.Upper = upper;
+        }
+
+        public int Lower { get; set; }
+        public int Upper { get; set; }
+    }
+}
---
+++ MergingInterval/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MergingInterval")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MergingInterval")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2032d79e-bb1d-48a6-8f2a-6da0d56205c7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
