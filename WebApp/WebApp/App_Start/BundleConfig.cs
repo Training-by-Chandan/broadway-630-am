2d32693fd7a0a564f47bcf517f9c7c0657fb6f2c
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-11-24 08:13:37 +0545

    few themes integration

--- WebApp/WebApp/App_Start/BundleConfig.cs
+++ WebApp/WebApp/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace WebApp
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new Bundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
--- WebApp/WebApp/App_Start/RouteConfig.cs
+++ WebApp/WebApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
